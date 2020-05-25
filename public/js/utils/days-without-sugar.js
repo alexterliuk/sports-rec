6301300c01caa3895eb9867fea2ad80990cc5c87
Alex Terliuk <alex.terliuk@gmail.com>
2020-05-25 18:40:58 +0300

    refactor: rewire js files

--- public/js/utils/days-without-sugar.js
+++ public/js/utils/days-without-sugar.js
@@ -1,31 +1,4 @@
 /**
- * showDaysWithoutWorkout
- */
-function showDaysWithoutWorkout(id) {
-  const days = [];
-
-  setTimeout(() => {
-    const tbody = document.getElementById('tbody');
-
-    if (tbody) {
-      for (let i = 0, qtyOfRows = tbody.childElementCount; i < qtyOfRows; i++) {
-        const row = tbody.children[i];
-        const cells = [];
-
-        for (let y = 0, qtyOfCells = row.childElementCount; y < qtyOfCells; y++) {
-          if (row.children[y].textContent) cells.push(row.children[y].textContent);
-        }
-
-        if (cells.length === 1) days.push(cells[0]);
-      }
-
-      const elem = document.getElementById(id);
-      if (elem) elem.textContent = `No workout (${days.length}) - ${days.join(', ')}.`;
-    }
-  }, 50);
-}
-
-/**
  * showDaysWithoutSugar
  */
 function showDaysWithoutSugar(id) {
