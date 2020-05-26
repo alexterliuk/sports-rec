e02ba073bf3bdbd048ee00cd823df212a7ca7e51
Alex Terliuk <alex.terliuk@gmail.com>
2020-05-26 21:45:14 +0300

    feat: add sign in and sign up

---
+++ public/js/utils/toggle-sign-mode.js
@@ -0,0 +1,31 @@
+for (const modeButton of document.getElementsByClassName('toggle-sign-mode')) {
+  modeButton.addEventListener('click', event => {
+    const id = event.target.id;
+
+    const signInPanel = document.getElementById('signInPanel');
+    const signInUsername = document.getElementById('signInUsername');
+    const signInPassword = document.getElementById('signInPassword');
+    const signInShowHide = document.querySelector('#signInPanel .toggle-password-view');
+
+    const signUpPanel = document.getElementById('signUpPanel');
+    const signUpUsername = document.getElementById('signUpUsername');
+    const signUpPassword = document.getElementById('signUpPassword');
+    const signUpShowHide = document.querySelector('#signUpPanel .toggle-password-view');
+
+    if (id === 'needSignUp') {
+      signInPanel.style.marginLeft = '300px';
+      signInUsername.value = '';
+      signInPassword.value = '';
+      signInPassword.setAttribute('type', 'password');
+      signInShowHide.textContent = 'show';
+      signUpPanel.style.marginTop = '-83px';
+    } else {
+      signUpPanel.style.marginTop = '-166px';
+      signUpUsername.value = '';
+      signUpPassword.value = '';
+      signUpPassword.setAttribute('type', 'password');
+      signUpShowHide.textContent = 'show';
+      signInPanel.style.marginLeft = '0';
+    }
+  });
+}
