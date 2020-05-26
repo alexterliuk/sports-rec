3b4496ceecd7e82bfd0c8f326f3af397178226ca
Alex Terliuk <alex.terliuk@gmail.com>
2020-05-27 01:53:09 +0300

    fix: logging forms switch for small screen

--- public/js/utils/toggle-sign-mode.js
+++ public/js/utils/toggle-sign-mode.js
@@ -13,14 +13,14 @@ for (const modeButton of document.getElementsByClassName('toggle-sign-mode')) {
     const signUpShowHide = document.querySelector('#signUpPanel .toggle-password-view');
 
     if (id === 'needSignUp') {
-      signInPanel.style.marginLeft = '300px';
+      signInPanel.style.marginLeft = '600px';
       signInUsername.value = '';
       signInPassword.value = '';
       signInPassword.setAttribute('type', 'password');
       signInShowHide.textContent = 'show';
       signUpPanel.style.marginTop = '-83px';
     } else {
-      signUpPanel.style.marginTop = '-166px';
+      signUpPanel.style.marginTop = '-332px';
       signUpUsername.value = '';
       signUpPassword.value = '';
       signUpPassword.setAttribute('type', 'password');
