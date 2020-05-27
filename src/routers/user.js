447f63c191274f12976c1df6c620b23fc4984e5a
Alex Terliuk <alex.terliuk@gmail.com>
2020-05-27 17:09:26 +0300

    feat: add /sign-up user route

---
+++ src/routers/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = new express.Router();
+const User = require('../models/user');
+
+// Create user
+router.post('/sign-up', async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+module.exports = router;
