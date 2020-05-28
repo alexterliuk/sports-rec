a973d7eb7082895dfbf581a126d0cf560357439a
Alex Terliuk <alex.terliuk@gmail.com>
2020-05-28 20:44:56 +0300

    feat: use mongoose, add user router to app.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+require('./src/db/mongoose');
+const userRouter = require('./src/routers/user');
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(userRouter);
 
 const publicDirectoryPath = path.join(__dirname, '/public');
-
 app.use(express.static(publicDirectoryPath));
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
