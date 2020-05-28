6074431c3f2c4d6688114ea6f52fc4d7bb821fcc
Alex Terliuk <alex.terliuk@gmail.com>
2020-05-28 22:17:52 +0300

    feat: split sign-up listener -> sign-up and _showLogInPanel

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,12 @@ const querySel = sel => document.querySelector(sel);
 function $listenToServerResponses(response, emitter, emitType) {
   const lib = {
     'sign-up'() {
+      querySel('#logInPanel span:first-child').textContent = `Welcome, ${pickElem('signUpUsername').value}.`;
+      this._showLogInPanel();
+    },
+    _showLogInPanel() {
       pickElem('signUpPanel').style.display = 'none';
       pickElem('signInPanel').style.display = 'none';
-      querySel('#logInPanel span:first-child').textContent = `Welcome, ${pickElem('signUpUsername').value}.`;
       pickElem('logInPanel').style.display = 'block';
     }
   };
