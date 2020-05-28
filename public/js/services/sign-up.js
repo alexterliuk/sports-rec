91a0017df0b64f16282f1c38da08c35f69f8ec71
Alex Terliuk <alex.terliuk@gmail.com>
2020-05-28 20:33:39 +0300

    feat: add sign-up a user service

--- public/js/services/sign-up.js
+++ public/js/services/sign-up.js
@@ -3,14 +3,32 @@ const signUpForm = document.querySelector('#signUpPanel form');
 signUpForm.addEventListener('submit', async event => {
   event.preventDefault();
 
-  const name = document.getElementById('signUpUsername').value;
-  const password = document.getElementById('signUpPassword').value;
+  const name = pickElem('signUpUsername').value;
+  const password = pickElem('signUpPassword').value;
+  const errElem = querySel('#signUpPanel .err-msg');
 
-  const response = await fetch('http:/sign-up', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ name, password }),
-  });
+  if (!errElem.classList.value.includes('active-error')) {
+    if (!name || !password) {
+      const message = 'Name and password fields are required.';
+      showError(message, errElem);
+
+    } else {
+      const response = await fetch('http:/sign-up', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, password }),
+      });
+
+      if (response.status === 400) {
+        const message = 'User saving failed. Only letters, numbers and underscores allowed. Max length of name - 20 chars, min length of password - 6 chars.';
+        showError(message, errElem);
+      }
+
+      if (response.status === 201) {
+        $emit(response, signUpForm, 'sign-up');
+      }
+    }
+  }
 });
