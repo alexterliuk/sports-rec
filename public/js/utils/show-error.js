7596b5525d24478f656777a932c064192c1731d7
Alex Terliuk <alex.terliuk@gmail.com>
2020-05-29 13:46:45 +0300

    feat: disallow click of need link if error is shown

--- public/js/utils/show-error.js
+++ public/js/utils/show-error.js
@@ -3,9 +3,14 @@ function showError(message, errElem) {
   errElem.classList.add('active-error');
   errElem.style.display = 'inline-block';
 
+  const prevElem = errElem.previousElementSibling;
+  const needLink = pickElem(prevElem.id.slice(0, 6) === 'signIn' ? 'needSignUp' : 'needSignIn');
+  needLink.classList.add('not-clickable');
+
   errElem.children[1].addEventListener('click', () => {
     errElem.style.display = '';
     errElem.children[0].textContent = '';
     errElem.classList.remove('active-error');
+    needLink.classList.remove('not-clickable');
   });
 }
--- public/js/utils/toggle-sign-mode.js
+++ public/js/utils/toggle-sign-mode.js
@@ -12,20 +12,23 @@ for (const modeButton of document.getElementsByClassName('toggle-sign-mode')) {
     const signUpPassword = document.getElementById('signUpPassword');
     const signUpShowHide = document.querySelector('#signUpPanel .toggle-password-view');
 
-    if (id === 'needSignUp') {
-      signInPanel.style.marginLeft = '600px';
-      signInUsername.value = '';
-      signInPassword.value = '';
-      signInPassword.setAttribute('type', 'password');
-      signInShowHide.textContent = 'show';
-      signUpPanel.style.marginTop = '-83px';
-    } else {
-      signUpPanel.style.marginTop = '-332px';
-      signUpUsername.value = '';
-      signUpPassword.value = '';
-      signUpPassword.setAttribute('type', 'password');
-      signUpShowHide.textContent = 'show';
-      signInPanel.style.marginLeft = '0';
+    if (!pickElem(id).classList.value.includes('not-clickable')) {
+      if (id === 'needSignUp') {
+        signInPanel.style.marginLeft = '600px';
+        signInUsername.value = '';
+        signInPassword.value = '';
+        signInPassword.setAttribute('type', 'password');
+        signInShowHide.textContent = 'show';
+        signUpPanel.style.marginTop = '-83px';
+
+      } else {
+        signUpPanel.style.marginTop = '-332px';
+        signUpUsername.value = '';
+        signUpPassword.value = '';
+        signUpPassword.setAttribute('type', 'password');
+        signUpShowHide.textContent = 'show';
+        signInPanel.style.marginLeft = '0';
+      }
     }
   });
 }
