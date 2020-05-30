2ba12786ab416e9af21af240ae865492b9784623
Alex Terliuk <alex.terliuk@gmail.com>
2020-05-30 23:02:27 +0300

    chore: cleanup

--- public/js/utils/toggle-password-view.js
+++ public/js/utils/toggle-password-view.js
@@ -2,8 +2,6 @@ for (const showButton of document.getElementsByClassName('toggle-password-view')
   showButton.addEventListener('click', event => {
     const passwordElement = document.getElementById(event.target.previousElementSibling.id);
     const node = event.target.childNodes[0];
-    console.log('node:', node);
-    console.log('event.target.previousElementSibling.id:', event.target.previousElementSibling.id);
     const content = node.textContent;
 
     if (node.textContent === 'show') {
