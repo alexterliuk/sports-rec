26ed67c8beddc270c1d34e1e6d77fb092b2a0aee
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-07 22:47:53 +0300

    refactor(parsedCssVars): collect more info, object -> array

--- public/js/globals.js
+++ public/js/globals.js
@@ -18,8 +18,8 @@ const logInPanel = pickElem('logInPanel');
 const welcomeMessage = pickElem('welcomeMessage');
 
 const parsedCssVars = (() => {
-  const parsedVars = {};
-  parsedVars.oneRem = parseInt(window.getComputedStyle(querySel('html')).fontSize, 10);
+  const oneRem = parseInt(window.getComputedStyle(querySel('html')).fontSize, 10);
+  const parsedVars = [];
 
   for (const stylesheet of document.styleSheets) {
     if (stylesheet.href.includes('variables.css')) {
@@ -30,12 +30,41 @@ const parsedCssVars = (() => {
             .filter(val => val)
             .forEach(val => {
               const entry = val.split(': ');
-              parsedVars[entry[0]] = entry[1];
+              const isDotAtStart = val => val[0] === '.';
+
+              parsedVars.push({
+                varKey: entry[0],
+                varVal: entry[1],
+                vals: entry[1].trim().split(' ').map(v => {
+                  const result = {};
+                  result.val = v.includes('var') ? v.split(/var\(|\)/)[1] : parseFloat(isDotAtStart(v) ? `0${v}` : v);
+                  result.cssUnit = v.includes('var') ? '' : v === '0' ? 'px' : v.slice(v.search(/[a-z]/));
+
+                  result.px = !result.val ? 0 : typeof result.val === 'string' ? null :
+                    result.cssUnit === 'px' ? result.val : result.val * oneRem;
+
+                  return result;
+                }),
+              });
             });
         }
       }
     }
   }
+
+  // if instead of number a val is another CSS var, replace it with corresponding value of that var
+  parsedVars.forEach(parsedVar => {
+    const pointersToAnotherCssVars = parsedVar.vals.filter(v => typeof v.val === 'string');
+
+    pointersToAnotherCssVars.forEach(pointer => {
+      const foundCssVar = parsedVars.find(cssVar => cssVar.varKey === pointer.val);
+      pointer.val = foundCssVar.vals[0].val;
+      pointer.cssUnit = foundCssVar.vals[0].cssUnit;
+      pointer.px = foundCssVar.vals[0].px;
+    });
+  });
+
+  parsedVars.push({ varKey: 'oneRem', varVal: oneRem });
 
   return parsedVars;
 })();
