0e2c63141e99afe2bd2d0aeecfd49b6078499885
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-10 23:38:53 +0300

    feat: add create-table-container.js

---
+++ public/js/table/create-table-container.js
@@ -0,0 +1,36 @@
+/**
+ * Create page section where a table will reside.
+ */
+function createTableContainer() {
+  const containerId = (() => {
+    let mtbNum = 1;
+    while (pickElem(`mtb${mtbNum}`)) mtbNum++;
+    return mtbNum;
+  })();
+
+  const params = {
+    parentId: 'mainTableBlock',
+    contId: `mtb${containerId}`,
+  };
+
+  const tableId = `${params.contId}Table`;
+
+  params.elems = [
+    { parentId: params.contId, tagName: 'h2', class: ['table-title'], text: `Table ${containerId}` },
+    { parentId: params.contId, tagName: 'div', class: ['buttons-block'], $name: 'buttons-block' },
+    { $parentName: 'buttons-block', tagName: 'button',
+      onClick: { funcName: 'addRow', funcArgs: [tableId] },
+      text: 'Add row',
+    },
+    { $parentName: 'buttons-block', tagName: 'button',
+      onClick: { funcName: 'addColumn', funcArgs: [tableId] },
+      text: 'Add column',
+    },
+    { parentId: params.contId, tagName: 'div', class: ['table-block'], $name: 'table-block' },
+    { $parentName: 'table-block', tagName: 'section', class: ['side-panel', 'left-panel'] },
+    { $parentName: 'table-block', tagName: 'section', class: ['table-panel'] },
+    { $parentName: 'table-block', tagName: 'section', class: ['side-panel', 'right-panel'] },
+  ];
+
+  buildDOM(params);
+}
