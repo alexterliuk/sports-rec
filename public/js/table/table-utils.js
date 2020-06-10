3b94edb0b473c4c81b38ac639c950f01ee8f1413
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-11 00:56:22 +0300

    feat: add createDelStick

--- public/js/table/table-utils.js
+++ public/js/table/table-utils.js
@@ -82,3 +82,19 @@ function alignTextAreasHeight(cell, txtArHeight) {
     }
   }
 }
+
+/**
+ * Create a span used as button for deleting row or column.
+ * @param {string} title
+ * @param {function} callback
+ * @returns {HTMLSpanElement}
+ */
+function createDelStick(title, callback) {
+  const delStick = document.createElement('span');
+  delStick.classList.add('delete-stick');
+  delStick.setAttribute('title', title);
+  delStick.setAttribute('role', 'button');
+  delStick.addEventListener('click', callback);
+
+  return delStick;
+}
