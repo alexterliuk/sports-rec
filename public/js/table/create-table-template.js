232eb4fa00d5f69349c6def0304d317b92369e53
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-11 23:06:35 +0300

    feat: create template table from specs

--- public/js/table/create-table-template.js
+++ public/js/table/create-table-template.js
@@ -1,9 +1,41 @@
-function createTableTemplate({ parentSelector, contId, rowsQty, colsQty }) {
+/**
+ * Create table within table-container.
+ * @param parentSelector {string}
+ * @param contId {string}
+ * @param colsQty {number}
+ * @param rowsQty {number}
+ */
+function createTableTemplate({ parentSelector, contId, colsQty, rowsQty }) {
   const params = {
     parentSelector,
     contId,
     tagName: 'table',
   };
+
+  const tableId = params.contId;
+
+  params.elems = [
+    { parentId: params.contId, tagName: 'thead', $name: 'thead' },
+    { $parentName: 'thead', tagName: 'tr', $name: 'thead-tr' },
+    { $parentName: 'thead-tr', tagName: 'th',
+      onClick: { funcName: 'sortColumn' },
+      onHover: { funcName: 'highlightColumn' },
+      multiple: {
+        qty: colsQty,
+        newIds: ['col1', 'col2', 'col3'],
+        columnsIds: true,
+        textRow: { col1: 'Col 1', col2: 'Col 2', col3: 'Col 3' },
+      },
+    },
+    { parentId: params.contId, tagName: 'tbody', $name: 'tbody' },
+    { $parentName: 'tbody',
+      builder: {
+        funcName: 'addRow',
+        funcArgs: [{ tableId }],
+        callsQty: rowsQty,
+      },
+    },
+  ];
 
   buildDOM(params);
 }
