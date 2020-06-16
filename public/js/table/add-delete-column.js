51f88a2e94168d8509969f8ca068baedbd6516f8
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-16 15:02:19 +0300

    feat: modify addColumn, deleteColumn to be main building table functions

--- public/js/table/add-delete-column.js
+++ public/js/table/add-delete-column.js
@@ -1,20 +1,45 @@
 /**
  * Add new column to table.
- * @param {string} id
+ * @param {HTMLButtonElement} btn
+ * @param {object} spec
+ * @param {object} dom - buildDOMLibrary
  */
-function addColumn(id) {
-  const tablePanel = pickElem(id).parentElement;
-  const hyphenId = pickElem(id).dataset.hyphenId;
-  const theadRow = querySel(`#${id} thead tr`);
-  const tbody = querySel(`#${id} tbody`);
+function addColumn(btn, spec, dom) {
+  console.log('addColumn spec:', spec);
 
+  if (!spec.tableId) {
+    console.error('Failed to add column, because no tableId provided.');
+    return;
+  }
+
+  const tablePanel = pickElem(spec.tableId).parentElement;
+  const hyphenId = pickElem(spec.tableId).dataset.hyphenId;
+  const theadRow = querySel(`#${spec.tableId} thead tr`);
+  const tbody = querySel(`#${spec.tableId} tbody`);
   const th = document.createElement('th');
+  th.setAttribute('id', `col${theadRow.children.length}${hyphenId}`);
+  addTextareaAndHider(th);
+
+  const currentTable = tables.get(hyphenId);
+  Object.keys(spec).forEach(key => {
+    if (!currentTable.hasOwnProperty(key)) currentTable[key] = spec[key];
+  });
+
+  th.append(createEditMask());
   th.append(createDelStick('Delete column', deleteColumn));
+
+  (dom || getBuildDOMLibrary()).hangOnElem(th, { onClick, onHover, newIds, textRow } = currentTable);
   theadRow.append(th);
 
-  for (const row of tbody.children) {
-    const cellId = `r${row.rowIndex}c${row.children.length}${hyphenId}`;
-    createCell(row, cellId);
+  if (theadRow.children.length === 1) {
+    for (let i = 0, { rowsQty } = tables.getAllConfig(); i < rowsQty; ++i) {
+      addRow(null, { tableId } = spec, getBuildDOMLibrary());
+    }
+  } else {
+    for (const row of tbody.children) {
+      const cellId = `r${row.rowIndex}c${row.children.length}${hyphenId}`;
+      createCell(row, cellId);
+    }
   }
 
   toggleScrollMode(tablePanel);
@@ -27,10 +52,22 @@ function deleteColumn() {
   const th = this.parentElement;
   const cellIndex = th.cellIndex;
   const tbody = th.parentElement.parentElement.nextElementSibling;
+  const theadRow = (thead => {
+    for (let child of thead.children) {
+      if (child.tagName === 'TR') return child;
+    }
+  })(th.parentElement.parentElement);
 
   th.parentElement.removeChild(th);
 
   for (const row of tbody.children) {
     row.removeChild(row.children[cellIndex]);
+    if (!cellIndex && theadRow.children.length) {
+      row.children[cellIndex].append(createDelStick('Delete row', deleteRow));
+    }
+  }
+
+  if (!theadRow.children.length) {
+    while (tbody.children.length) tbody.deleteRow(-1);
   }
 }
