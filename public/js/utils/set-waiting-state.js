710323c37e3d437b2af4d059738f7ac21d269d2c
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-22 18:21:41 +0300

    feat: add setWaitingState

---
+++ public/js/utils/set-waiting-state.js
@@ -0,0 +1,26 @@
+/**
+ * Start or finish waiting state to indicate client <-> server interaction.
+ * @param {boolean} type
+ * @param {string} tableId
+ */
+function setWaitingState(type, tableId) {
+  const method = type ? 'add' : 'remove';
+  const pageSegmentId = tableId.slice(0, -5);
+  const tableTitle = querySel(`#${pageSegmentId} .table-title`);
+  const buttonsBlock = querySel(`#${pageSegmentId} .buttons-block`);
+  const tableBlock = querySel(`#${pageSegmentId} .table-block`);
+
+  for (const elem of [tableTitle, { processChildren: buttonsBlock, spinner: buttonsBlock }, tableBlock]) {
+    if (!elem.processChildren) elem.classList[method]('semitransparent');
+
+    if (elem.processChildren) {
+      const parent = elem.processChildren;
+
+      for (const child of parent.children) {
+        child.classList[method]('semitransparent');
+      }
+    }
+
+    if (elem.spinner) elem.spinner.classList[method]('spinner');
+  }
+}
