8880935df9472b1baa85ead5117df90342c9e27c
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-25 12:17:34 +0300

    feat: add table router to app.js

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require('path');
 const express = require('express');
 require('./src/db/mongoose');
 const userRouter = require('./src/routers/user');
+const tableRouter = require('./src/routers/table');
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use(tableRouter);
 
 const publicDirectoryPath = path.join(__dirname, '/public');
 app.use(express.static(publicDirectoryPath));
