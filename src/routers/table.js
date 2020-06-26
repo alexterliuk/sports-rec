cdc41941a9301f615daebcc3ce5063a782951f2c
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-27 01:02:33 +0300

    feat: prevent saving table with same hyphenId for a user

--- src/routers/table.js
+++ src/routers/table.js
@@ -9,6 +9,14 @@ const validateTypesInTable = require('../utils/validate-types-in-table');
 router.post('/table', auth, async (req, res) => {
   req.body.owner = req.session.userId;
 
+  const tablesWithSameHyphenId = await Table.find({ hyphenId: req.body.hyphenId });
+  const tableOfCurrentUser = tablesWithSameHyphenId.find(table => table.owner.toString() === req.session.userId);
+
+  if (tableOfCurrentUser) {
+    return res.status(409)
+              .send({ error: 'Table cannot be saved - another table with same hyphenId already stored in database.' });
+  }
+
   if (!validateTypesInTable(req.body)) {
     return res.status(400)
               .send({ error: 'Some type in the table is different from what specified in table schema.'});
