16b68d422d378d67a152ae894fe359cd914f71c6
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-26 22:11:59 +0300

    refactor(table router): ad hoc check for signed in user -> auth middleware

--- src/routers/table.js
+++ src/routers/table.js
@@ -1,33 +1,27 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const Table = require('../models/table');
 const validateTypesInTable = require('../utils/validate-types-in-table');
 
 // Create table
-router.post('/table', async (req, res) => {
-  const user = await User.findById(req.session.userId);
+router.post('/table', auth, async (req, res) => {
+  req.body.owner = req.session.userId;
 
-  if (user) {
-    req.body.owner = req.session.userId;
+  if (!validateTypesInTable(req.body)) {
+    return res.status(400)
+              .send({ error: 'Some type in the table is different from what specified in table schema.'});
+  }
 
-    if (!validateTypesInTable(req.body)) {
-      return res.status(400)
-                .send({ error: 'Some type in the table is different from what specified in table schema.'});
-    }
+  const table = new Table(req.body);
 
-    const table = new Table(req.body);
+  try {
+    await table.save();
+    res.send({ id: table._id });
 
-    try {
-      await table.save();
-      res.send({ id: table._id });
-
-    } catch (error) {
-      res.status(400).send(error);
-    }
-
-  } else {
-    res.status(401).send({ error: 'Please authorize.' });
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
