0e45fc7b36aade80f0051d962b4e9ffc3366b23b
Alex Terliuk <alex.terliuk@gmail.com>
2020-06-28 12:10:18 +0300

    refactor: move validating types to pre save Table middleware

--- src/models/table.js
+++ src/models/table.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validateIdEndingsInTable = require('../utils/validate-id-endings-in-table');
+const validateTypesInTable = require('../utils/validate-types-in-table');
 
 const stringTypeReq = { type: String, required: true };
 const arrayTypeReq = { type: Array, required: true };
@@ -39,10 +40,15 @@ const tableSchema = new mongoose.Schema({
   timeStamps: true,
 });
 
-// All hyphenId endings in table must be equal, non unique id is forbidden
 tableSchema.pre('save', function(next) {
+  // All hyphenId endings in table must be equal, non unique id is forbidden
   if (!validateIdEndingsInTable(this)) {
     throw new Error('Non unique id in table detected.');
+  }
+
+  // Table must contain proper types
+  if (!validateTypesInTable(this)) {
+    throw new Error('Some type in the table is different from what specified in table schema.');
   }
 
   next();
--- src/routers/table.js
+++ src/routers/table.js
@@ -3,7 +3,6 @@ const router = new express.Router();
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const Table = require('../models/table');
-const validateTypesInTable = require('../utils/validate-types-in-table');
 
 // Create table
 router.post('/table', auth, async (req, res) => {
@@ -15,11 +14,6 @@ router.post('/table', auth, async (req, res) => {
   if (tableOfCurrentUser) {
     return res.status(409)
               .send({ error: 'Table cannot be saved - another table with same hyphenId already stored in database.' });
-  }
-
-  if (!validateTypesInTable(req.body)) {
-    return res.status(400)
-              .send({ error: 'Some type in the table is different from what specified in table schema.'});
   }
 
   const table = new Table(req.body);
