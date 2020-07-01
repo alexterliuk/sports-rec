5572f376a7aed276f834a6da5ae291c8ad730e8a
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-01 16:06:11 +0300

    feat: add makeQueryStrings

---
+++ public/js/utils/make-query-strings.js
@@ -0,0 +1,29 @@
+/**
+ * Make and concatenate query strings with & in-between.
+ * @param {object} options - e.g. { limit: 10, skip: 20 }
+ * @param {array} queryStringsKeys - keys to look for into options and make query strings, e.g. ['limit']
+ * @param {string} makingType - type an options[key] value expected to be of before converting to query string
+ * @returns {string} - query string | query strings united in one string | ''
+ */
+function makeQueryStrings(options, queryStringsKeys, makingType) {
+  const makingTypes = ['number'];
+
+  if (typeof options !== 'object' || !makingTypes.includes(makingType)) return '';
+
+  const makeQueryStringFrom = {
+    number: key => (int => int > 0 && `${key}=${int}`)(parseInt(data.opts[key])),
+  };
+
+  const data = (() => {
+    const keys = Array.isArray(queryStringsKeys) && queryStringsKeys.length && queryStringsKeys || Object.keys(options);
+    const opts = {};
+
+    for (const key of keys) opts[key] = options[key];
+
+    return Object.keys(opts).length ? { opts, keys } : false;
+  })();
+
+  const queryStrings = data && data.keys.map(key => makeQueryStringFrom[makingType](key));
+
+  return (queryStrings || []).filter(val => val).join('&');
+}
