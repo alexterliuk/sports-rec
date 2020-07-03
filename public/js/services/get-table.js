6e23f9ce1f92a6816cce5993fcbc443328fd25f8
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-03 19:19:59 +0300

    refactor(getTable): make destructuring not crash if no id, return {} or null

--- public/js/services/get-table.js
+++ public/js/services/get-table.js
@@ -3,7 +3,8 @@
  * @param {HTMLButtonElement} btn
  * @param {string} id
  */
-async function getTable(btn, id) {
+async function getTable(btn, { id } = {}) {
+  if (!id) return null;
   setWaitingState(true, { id: 'dashboardBlock' });
 
   const response = await fetch(`http:/tables/${id}`, {
@@ -12,11 +13,11 @@ async function getTable(btn, id) {
 
   if (response.status === 200 || response.status === 304) {
     setWaitingState(false, { id: 'dashboardBlock' });
-    const _response = await response.json();
-    buildTables([_response]);
+    return response.json();
 
   } else {
     setWaitingState(false, { id: 'dashboardBlock' });
     notify('dashboardInfo', 'Table not found. Make sure you search own table, not another user\'s one.', 'error', 5000);
+    return null;
   }
 }
