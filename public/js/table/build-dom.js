2992072f632c3617b975287a3dea7096454e75f2
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-09 22:08:33 +0300

    chore: remove redundant tableOuterHTML from table

--- public/js/table/build-dom.js
+++ public/js/table/build-dom.js
@@ -377,7 +377,6 @@ function buildDOM(data) {
 
       if (data.tagName === 'table') {
         const table = dom.root.element;
-        tables.addToTable(hyphenId, { tableOuterHTML: table.outerHTML }, true);
         watch('pristine', table);
 
         dom.root.theadRow = collectRowsData(table.children[0]);
