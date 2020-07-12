a593635f63af684d2c238f5b5a454f82af1cd809
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-12 16:28:42 +0300

    fix(updateIfChanged): renew saved array with old, new values, exclude doubles

--- src/utils/update-if-changed.js
+++ src/utils/update-if-changed.js
@@ -7,41 +7,67 @@ function updateIfChanged(existingTable, reqBody) {
   const cellKeys = ['id', 'textareaValue'];
   const cellKeysArr = ['classNames', 'styles', 'textareaStyles'];
 
-  _modify(existingTable, reqBody, 'tableTitle');
-  _modifyArr(existingTable, reqBody, 'classNames');
-  _compareRows(existingTable, reqBody, 'theadRow');
+  modify(existingTable, reqBody, 'tableTitle');
+  modifyArr(existingTable, reqBody, 'classNames');
+  compareRows(existingTable, reqBody, 'theadRow');
 
   reqBody.tbodyRows.forEach((newRow, idx) => {
     const existingRow = existingTable.tbodyRows[idx];
 
-    _modify(existingRow, newRow, 'id');
-    _compareRows(existingRow, newRow, 'cells');
+    modify(existingRow, newRow, 'id');
+    compareRows(existingRow, newRow, 'cells');
   });
 
-  // for each cell in rows invoke _modify and _modifyArr
-  function _compareRows(oldData, newData, arrKey) {
+  // for each cell in rows invoke modify and modifyArr
+  function compareRows(oldData, newData, arrKey) {
     newData[arrKey].forEach((newCell, idx) => {
       const existingCell = oldData[arrKey][idx];
 
-      cellKeys.forEach(key => { _modify(existingCell, newCell, key); });
-      cellKeysArr.forEach(key => { _modifyArr(existingCell, newCell, key); });
+      cellKeys.forEach(key => { modify(existingCell, newCell, key); });
+      cellKeysArr.forEach(key => { modifyArr(existingCell, newCell, key); });
     });
   }
 
-  function _modifyArr(a, b, arrKey) {
-    // iterate over array of new table (b), because it might contain more values, than array of existing table (a)
-    b[arrKey].forEach((val, idx) => {
-      if (!a[arrKey].includes(b[arrKey][idx])) {
-        a[arrKey].push(b[arrKey][idx]);
-      }
+  function modifyArr(a, b, arrKey) {
+    const newValues = []; // classNames, styles or textareaStyles
+
+    for (let i = b[arrKey].length - 1; i >= 0; i--) {
+      // exclude possible same values occurrence
+      arrKey !== 'classNames' ?
+        addValueIfNotExist(newValues, b[arrKey], i, 'name') :
+        addValueIfNotExist(newValues, b[arrKey], i);
+    }
+
+    // add new values to saved array
+    newValues.forEach((val, idx) => {
+      a[arrKey].splice(idx, 1, val);
     });
+
+    // remove any values in saved array that new array doesn't have
+    if (newValues.length) {
+      a[arrKey].length = newValues.length;
+
+    } else {
+      // assigning zero length doesn't work (values will remain in db), so remove values one by one
+      let stop = 0;
+      while (a[arrKey].length) {
+        a[arrKey].pop();
+        if (++stop === 1000) break;
+      }
+    }
   }
 
   // if target's value is different, replace it with modifier's value
-  function _modify(target, modifier, key) {
+  function modify(target, modifier, key) {
     if (target[key] !== modifier[key]) {
       target[key] = modifier[key];
     }
+  }
+
+  // if target has no such value, add it
+  function addValueIfNotExist(target, arr, i, key) {
+    const valueNotExist = !target.find(val => key ? val[key] === arr[i][key] : val === arr[i]);
+    if (valueNotExist) target.unshift(arr[i]);
   }
 }
 
