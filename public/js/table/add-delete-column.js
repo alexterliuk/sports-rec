c534569eebefc3b9f8ce87a272b83c0b28ce4310
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-13 16:14:51 +0300

    fix: add sorting-button to new column

--- public/js/table/add-delete-column.js
+++ public/js/table/add-delete-column.js
@@ -49,7 +49,7 @@ function addColumn(btn, spec, dom) {
   th.append(createEditButton());
   th.append(createDelStick('Delete column', deleteColumn));
 
-  if ((spec.onClick || {}).funcName === 'sortColumn') {
+  if ((spec.onClick || currentTable.onClick || {}).funcName === 'sortColumn') {
     th.append(createSortingButton('Sort column', sortColumn));
   }
 
