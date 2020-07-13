a5543e8e985e4f11b2d438c1330847d934a81187
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-13 14:43:37 +0300

    feat: add createArbitraryString

---
+++ public/js/utils/create-arbitrary-string.js
@@ -0,0 +1,18 @@
+/**
+ * Create arbitrary string of specified length.
+ * @param {number} length
+ */
+function createArbitraryString(length) {
+  const alphabet = (alph => alph + alph + alph + alph)('abcdefghijklmnopqrstuvwxyz');
+  const getRandomIndex = () => +`${('' + Math.random()).slice(-2)}`;
+
+  return (() => {
+    let id = '';
+    for (let i = 0; i < length; i++) {
+      const index = getRandomIndex();
+      const char = index % 2 ? alphabet[index] : alphabet[index].toUpperCase();
+      id += char;
+    }
+    return id;
+  })();
+}
