5d6b5cf5bf33ba6e3fdd881357717dc71a07f101
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-15 23:51:54 +0300

    feat: add deleteTable

---
+++ public/js/services/delete-table.js
@@ -0,0 +1,31 @@
+/**
+ * Delete table in database.
+ * @param {HTMLButtonElement} btn
+ * @param {object} tableData
+ */
+async function deleteTable(btn, tableData) {
+  if (!tableData) return { deleted: false };
+
+  setWaitingState(true, tableData);
+
+  const response = await fetch(`http:/tables/${tableData.hyphenId}`, {
+    method: 'DELETE',
+  });
+
+  if (response.status === 200) {
+    setWaitingState(false, tableData);
+    notify(tableData.tableId, 'Table successfully deleted.', 'success', 3000);
+    return { deleted: true };
+
+  } else if (response.status === 404) {
+    setWaitingState(false, tableData);
+    notify(tableData.tableId, 'Table not found.', 'error', 3000);
+    return { deleted: false };
+  }
+
+  else {
+    setWaitingState(false, tableData);
+    notify(tableData.tableId, 'Failed to delete. Something went wrong.', 'error', 3000);
+    return { deleted: false };
+  }
+}
