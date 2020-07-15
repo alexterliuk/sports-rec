660f6b0c0b19435f87d80ae7b5fcb20b24a13460
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-15 18:42:48 +0300

    refactor: simplify isEmptyString

--- public/js/utils/is-empty-string.js
+++ public/js/utils/is-empty-string.js
@@ -1,12 +1,8 @@
 /**
- * Test whether string is empty, or composed only of whitespaces.
+ * Strip off whitespaces and test whether string is empty.
  * @param {string} str
  * @returns {boolean}
  */
 function isEmptyString(str) {
-  if (typeof str !== 'string') return false;
-
-  // true if any character inside
-  const notEmptyString = str.split('').some(v => !/\s/.test(v));
-  return !notEmptyString;
+  return typeof str !== 'string' ? false : !str.trim().length;
 }
