a73921806172bfc3aa69ac169900fd3b511b6480
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-27 21:10:12 +0300

    feat(dashboardDriver): add addNavPageButtonClickListeners

--- public/js/dashboard/dashboard-driver.js
+++ public/js/dashboard/dashboard-driver.js
@@ -21,6 +21,11 @@ const dashboardDriver = (function() {
     dashboardPagination = pickElem('dashboardPagination');
     dashboardPages = pickElem('dashboardPages');
 
+    const navButtons = updateNavPageButtons('prevPage', 'nextPage');
+    prevPage = navButtons[0];
+    nextPage = navButtons[1];
+    addNavPageButtonClickListeners(prevPage, nextPage);
+
     if (!dashboardInfo || !dashboardPages) return;
 
     _data.pages = pages;
@@ -30,6 +35,34 @@ const dashboardDriver = (function() {
     _data.maxButtonsInRow = maxButtonsInRow;
 
     launched = true;
+  };
+
+  /**
+   * Add click event listener to prevPage, nextPage.
+   * @param {HTMLElement} elems
+   */
+  const addNavPageButtonClickListeners = (...elems) => {
+    for (const elem of elems) {
+      if (typeof elem === 'object' && elem instanceof HTMLElement) {
+        // make sure this is nav button, add functionality of navigation between pageButtons
+        if (['prevPage', 'nextPage'].includes(elem.id)) {
+          elem.addEventListener('click', event => {
+            const pageNum = +event.target.dataset.pageNum;
+
+            if (_data.pages[pageNum]) {
+              if (event.target.id === 'prevPage') {
+                prevPage.dataset.pageNum = pageNum === 1 ? 0 : pageNum - 1;
+                nextPage.dataset.pageNum = +nextPage.dataset.pageNum - 1;
+
+              } else {
+                nextPage.dataset.pageNum = pageNum + 1;
+                prevPage.dataset.pageNum = +prevPage.dataset.pageNum + 1;
+              }
+            }
+          });
+        }
+      }
+    }
   };
 
   return { launch, isLaunched };
