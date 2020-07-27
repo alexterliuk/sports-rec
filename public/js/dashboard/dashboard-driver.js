b38792a79a77fba716edb39d002094a050cfed7b
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-27 21:17:13 +0300

    feat(dashboardDriver): add refreshPageButtons

--- public/js/dashboard/dashboard-driver.js
+++ public/js/dashboard/dashboard-driver.js
@@ -1,6 +1,6 @@
 /**
  * Dashboard driver component. Responsible for creating, updating, deleting of data inside dashboard.
- * Dashboard consists of two main blocks - dashboardInfo, dashboardPage. Each is driven by corresponding update function.
+ * Dashboard consists of two main blocks - dashboardInfo, dashboardPages. Each is driven by corresponding update function.
  * Component is initialized by dashboardDriver.launch.
  */
 const dashboardDriver = (function() {
@@ -38,6 +38,35 @@ const dashboardDriver = (function() {
   };
 
   /**
+   * Refresh dashboardPages due to adding/deleting of a page or navigating between page buttons.
+   * @param {number} firstButtonNum
+   * @param {object} newPage
+   */
+  const refreshPageButtons = ({ firstButtonNum, newPage } = {}) => {
+    if (firstButtonNum) { // navigation buttons
+      if (firstButtonNum < +dashboardPages.children[0].dataset.pageNum) { // prevPage clicked
+        dashboardPages.children[dashboardPages.children.length - 1].remove();
+        dashboardPages.prepend(_data.pages[firstButtonNum].pageButton);
+
+      } else if (firstButtonNum > +dashboardPages.children[0].dataset.pageNum) { // nextPage clicked
+        const lastPageNum = +dashboardPages.children[dashboardPages.children.length - 1].dataset.pageNum;
+        dashboardPages.children[0].remove();
+        dashboardPages.append(_data.pages[lastPageNum + 1].pageButton);
+      }
+
+    } else if (newPage && dashboardPages.children.length < _data.maxButtonsInRow) {
+      dashboardPages.append(newPage.pageButton);
+
+    } else { // last page might have been deleted
+      let stop = 0;
+      while (dashboardPages.children.length && !_data.pages[dashboardPages.children.length]) {
+        dashboardPages.children[dashboardPages.children.length - 1].remove();
+        if (++stop === 1000) break;
+      }
+    }
+  };
+
+  /**
    * Add click event listener to prevPage, nextPage.
    * @param {HTMLElement} elems
    */
@@ -58,6 +87,8 @@ const dashboardDriver = (function() {
                 nextPage.dataset.pageNum = pageNum + 1;
                 prevPage.dataset.pageNum = +prevPage.dataset.pageNum + 1;
               }
+
+              refreshPageButtons({ firstButtonNum: +prevPage.dataset.pageNum + 1});
             }
           });
         }
