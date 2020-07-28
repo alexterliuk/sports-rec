02c55abb4aea732e1afa9638ebba6bc6769b6e69
Alex Terliuk <alex.terliuk@gmail.com>
2020-07-29 00:01:04 +0300

    feat(dashboardDriver): add skip param to makeDashboardPages

--- public/js/dashboard/make-dashboard-pages.js
+++ public/js/dashboard/make-dashboard-pages.js
@@ -1,14 +1,16 @@
 /** Fetch tables and make pages - each with pageButton, dashboard rows (.dbo-items), 'Build All These Tables' button.
  * ?@param {number} tablesQty - how many tables to fetch
+ * ?@param {number} skip - how many tables to skip
  * ?@param {number} maxTablesInDashboardPage
  * ?@param {number} maxButtonsInRow
  */
-async function makeDashboardPages({ tablesQty, maxTablesInDashboardPage, maxButtonsInRow } = {}) {
+async function makeDashboardPages({ tablesQty, skip, maxTablesInDashboardPage, maxButtonsInRow } = {}) {
   tablesQty = validatePositiveNumber(tablesQty);
+  skip = validatePositiveNumber(skip, 0);
   maxTablesInDashboardPage = validatePositiveNumber(maxTablesInDashboardPage, 10);
   maxButtonsInRow = validatePositiveNumber(maxButtonsInRow, 5);
 
-  const tables = await getUserTables(null, { limit: tablesQty || 50 });
+  const tables = await getUserTables(null, { limit: tablesQty || 50, skip });
   // const getTables = () => JSON.parse(JSON.stringify(tables));
 
   shownTablesInDashboard.add(tables.slice(0, 10));
