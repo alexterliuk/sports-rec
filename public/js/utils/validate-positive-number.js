8fe190fb9a73bce5c83c493a75eb82d76361557e
Alex Terliuk <alex.terliuk@gmail.com>
2020-08-12 21:13:23 +0300

    chore: edit preamble in validatePositiveNumber

--- public/js/utils/validate-positive-number.js
+++ public/js/utils/validate-positive-number.js
@@ -1,7 +1,8 @@
 /**
  * Return positive number or provided defaultValue, or false.
  * @param {number} val - any value
- * @param {number} defaultVal
+ * @param {number} [defaultVal]
+ * @returns {boolean|number} - false or number > 0
  */
 function validatePositiveNumber(val, defaultVal) {
   const valid = typeof val === 'number' && Number.isFinite(val) && val > 0 && val;
