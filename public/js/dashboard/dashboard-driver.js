9f893ad58a336cd484fb83d98909263915d5dabb
Alex Terliuk <alex.terliuk@gmail.com>
2020-08-15 22:22:12 +0300

    refactor: dashboard-page-buttons-utils.js -> ...-creating-utils.js

--- public/js/dashboard/dashboard-driver.js
+++ public/js/dashboard/dashboard-driver.js
@@ -2,7 +2,7 @@ import createDashboardItems from './create-dashboard-items.js';
 import createDashboardPagination from './create-dashboard-pagination.js';
 import updateDashboardIndexes from './dashboard-utils/update-dashboard-indexes.js';
 import { addDashboardPageToPages } from './dashboard-utils/dashboard-pages-utils.js';
-import { updateNavPageButtons } from './dashboard-utils/dashboard-page-buttons-utils.js';
+import { updateNavPageButtons } from './dashboard-utils/dashboard-page-buttons-creating-utils.js';
 import setActivePage from './dashboard-utils/set-active-page.js';
 import { visualizeWhileAppending, visualizeThenRemove } from './dashboard-utils/visualize.js';
 import getDefaultTimeoutDuration from '../utils/get-default-timeout-duration.js';
